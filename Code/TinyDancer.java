d51028d07aa1a0e40c5cc9ba22e77032ee114514
Urmikl18 <pavel.shevchenko.95@gmail.com>
2016-11-29 15:25:39 +0100

    Update TinyDancer.java
    Added: HTML parsing (extract text from body tag, title tag and summary tag) + Concurrent search of multiple fields

--- Code/TinyDancer.java
+++ Code/TinyDancer.java
@@ -23,11 +23,14 @@ import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.RAMDirectory;
+import org.jsoup.Jsoup;
 
 /**
  * Command line based Information Retrieval System Parses and indexes
  * .htm,.html,.txt files Uses Porter Stemmer Stop Word elimination Uses
  * inverted index Delivers a ranked list of files that fulfill the query
+ * @version IR_P01
+ * Uses lucene 6.3.0 and jsoup 1.10.1
  */
 public class TinyDancer {
 
@@ -57,8 +60,8 @@ public class TinyDancer {
 	 * 4. Initialize the indexer this.writer.
 	 * 
 	 * @param directory
-	 *            to be indexed
-	 * @throws IOException
+	 *            To be indexed
+	 * @throws IOException Not possible to create directory for index.
 	 */
 	public TinyDancer(String directory) throws IOException {
 		this.catalog = directory;
@@ -73,7 +76,7 @@ public class TinyDancer {
 	 * directory 3. .htm, .html, .txt files are added to the list.
 	 * 
 	 * @param file
-	 *            to be checked
+	 *            To be indexed
 	 */
 	private void addFiles(File file) {
 		if (!file.exists()) {
@@ -107,7 +110,7 @@ public class TinyDancer {
 	 * Indexes all the files 1. Adds all the files from the directory to the
 	 * list. 2. Parses all the files from this.files into this.indfiles
 	 * 
-	 * @throws IOException
+	 * @throws IOException Can't open the given folder.
 	 */
 	public void createIndex() throws IOException {
 		this.addFiles(new File(this.catalog));
@@ -120,20 +123,25 @@ public class TinyDancer {
 				}
 				Document d = new Document();
 				fr = new FileReader(f);
-				if (f.getName().endsWith(".txt")) {								//.txt search for content
-					d.add(new TextField("contents", fr));
-					d.add(new StringField("path", f.getAbsolutePath(),
+				if (f.getName().endsWith(".txt")) {
+					d.add(new StringField("name", f.getName(),Field.Store.YES));//.txt name
+					d.add(new StringField("path", f.getAbsolutePath(),			//path
 							Field.Store.YES));
-					d.add(new StringField("modified", DateTools.timeToString(
+					d.add(new StringField("modified", DateTools.timeToString(	//last modified date
 							f.lastModified(), DateTools.Resolution.SECOND),
 							Field.Store.YES));
-				} else {														//TODO: .html search for body, content, title
-					d.add(new TextField("contents", fr));
-					d.add(new StringField("path", f.getAbsolutePath(),
+					d.add(new TextField("contents", fr));						//main content
+				} else {
+					org.jsoup.nodes.Document html = Jsoup.parse(f,"UTF-8");
+					d.add(new StringField("name", f.getName(),Field.Store.YES));//.htm(l) name
+					d.add(new StringField("path", f.getAbsolutePath(),			//path
 							Field.Store.YES));
-					d.add(new TextField("modified", DateTools.timeToString(
+					d.add(new TextField("modified", DateTools.timeToString(	//last modified date
 							f.lastModified(), DateTools.Resolution.SECOND),
 							Field.Store.YES));
+					d.add(new TextField("contents", html.body().text(),Field.Store.YES)); //SHOULD BE: body tag | IS: text of all tags between <html></html>
+					d.add(new TextField("title", html.title(),Field.Store.YES));		//title tag
+					d.add(new StringField("summary", html.select("summary").text(),Field.Store.YES));	//summary tag for output
 				}
 				this.indfiles.add(d);
 				this.writer.addDocument(d);
@@ -141,6 +149,7 @@ public class TinyDancer {
 				if (this.verbose > 0) {
 					System.out.println("ERROR| Parsing file: "
 							+ f.getAbsolutePath() + " failed");
+					e.printStackTrace();
 				}
 			} finally {
 				fr.close();
@@ -153,7 +162,7 @@ public class TinyDancer {
 	 * Creates formatted output of the files stored in this.files 1. Number 2.
 	 * Full name 3. Last modified date
 	 * 
-	 * @throws ParseException
+	 * @throws ParseException Can't format the given date.
 	 */
 	public void listFiles() throws ParseException {
 		int i = 1;
@@ -172,23 +181,26 @@ public class TinyDancer {
 	}
 
 	/**
-	 * 1. open index to read
-	 * 2. create searcher to look up in index
-	 * 3. parse query
-	 * 4. store results in hits (sorted by score)
-	 * 5. formated output: results
+	 * 1. Open index to read
+	 * 2. Create searcher to look up in index
+	 * 3. Parse query (modify query to contents:QUERY OR title:QUERY OR modified:QUERY)
+	 * 4. Store results in hits (sorted by score)
+	 * 5. Formated output: results (fields to output based on the type of the file)
 	 * 
-	 * @param str
-	 * @throws IOException
-	 * @throws org.apache.lucene.queryparser.classic.ParseException
-	 * @throws ParseException
+	 * @param str Represents the query
+	 * @throws IOException Can't open the directory with index
+	 * @throws org.apache.lucene.queryparser.classic.ParseException Can't parse the given query
+	 * @throws ParseException Can't parse the given date
 	 */
 	public void searchFor(String str) throws IOException,
 			org.apache.lucene.queryparser.classic.ParseException,
 			ParseException {
 		IndexReader reader = DirectoryReader.open(this.dir);
 		IndexSearcher searcher = new IndexSearcher(reader);
-		Query query = new QueryParser("contents", analyzer).parse(str);
+		QueryParser queryParser = new QueryParser("contents", analyzer); //default
+		
+		String multiField = "contents:"+str+" OR title:"+str+" OR modified:"+str; //search contents, title, modified field concurrently
+		Query query=queryParser.parse(multiField);
 
 		TopDocs col = searcher.search(query, 10);
 		ScoreDoc[] hits = col.scoreDocs;
@@ -197,18 +209,34 @@ public class TinyDancer {
 		SimpleDateFormat sf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 
 		System.out.println("\nINFO| Found " + hits.length + " hits.");
-		System.out.format("%-4s | %-100s | %-20s | %-10s\n", "ID",
-				"Full file name", "Last modified date", "Score");
-		System.out
-				.println("------------------------------------------------------------------"
-						+ "---------------------------------------------------------------"
-						+ "------------");
+		
 		for (int i = 0; i < hits.length; ++i) {
 			int docId = hits[i].doc;
 			Document d = searcher.doc(docId);
-			System.out.format("%-4d | %-100s | %-20s | %-10f\n", (i + 1),
-					d.get("path"), sf.format(orig.parse(d.get("modified"))),
-					hits[i].score);
+			if (d.get("name").endsWith(".txt"))
+			{
+				System.out.println("Rank: "+(i+1));
+				System.out.println("\tType: TEXT");
+				System.out.println("\tName: "+d.get("name"));
+				System.out.println("\tPath: "+d.get("path"));
+				System.out.println("\tLast modification time: "+sf.format(orig.parse(d.get("modified"))));
+				System.out.println("\tScore: "+hits[i].score);
+			}
+			else
+			{
+				System.out.println("Rank: "+(i+1));
+				System.out.println("\tType: HTML");
+				System.out.println("\tName: "+d.get("name"));
+				System.out.println("\tPath: "+d.get("path"));
+				System.out.println("\tLast modification time: "+sf.format(orig.parse(d.get("modified"))));
+				System.out.println("\tScore: "+hits[i].score);
+				System.out.println("\tTitle: "+d.get("title"));
+				String sum=d.get("summary");
+				if (sum.equals(""))
+					System.out.println("\tSummary: The file contains no summary.");
+				else
+					System.out.println("\tSummary: "+sum);
+			}
 		}
 		reader.close();
 	}
@@ -222,39 +250,38 @@ public class TinyDancer {
 	 * 6. search for query
 	 * 7. return until input = 'q'
 	 * 
-	 * @param args
-	 * @throws IOException
+	 * @param args args[0] contains the path to the folder to be indexed
 	 */
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 		String query;
-		String dir = args[0];
+		String dir = "";
 		
 		try {
+			dir=args[0];
 			TinyDancer td = new TinyDancer(dir); // new irsystem
 			td.createIndex(); // index files
 			td.listFiles();
 			
-			System.out.println("Tip 'q' to quit \n Searching for: ");
+			System.out.println("Tip 'q' to quit \nSearching for: ");
 			query = sc.next();
 			while(!query.equals("q"))
 			{
 				td.searchFor(query);
-				System.out.println("\n Tip 'q' to quit \n Searching for: ");
+				System.out.println("\nTip 'q' to quit \nSearching for: ");
 				query = sc.next();
 			}
 			
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("ERROR| No directory was entered. Please restart with 'java -jar IR_P01.jar [path_to_folder]");
 		} catch (IOException e) {
 			System.out.println("ERROR| Could not index the directory: " + dir);
-			e.printStackTrace();
 		} catch (ParseException e) {
 			System.out.println("ERROR| Could not parse the date");
-			e.printStackTrace();
 		} catch (org.apache.lucene.queryparser.classic.ParseException e) {
 			System.out.println("ERROR| Could not parse the query");
-			e.printStackTrace();
 		}
 	}
-
+	
 }
